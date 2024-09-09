b99db33b713b695eb7b3422517e3f7be573b86d2
Bjørn <bjand49@gmail.com>
2024-09-09 11:58:02 +0200

    added gitignore and fetch

--- rawg/src/App.tsx
+++ rawg/src/App.tsx
@@ -1,8 +1,37 @@
 import { Grid, GridItem } from "@chakra-ui/react"
 import NavBar from "./components/navbar"
+import axios from "axios"
+import { useEffect, useState } from "react";
+
+const instance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  timeout: 1000,
+  params: {
+    key: import.meta.env.VITE_API_KEY
+  }
+});
+
+interface GameResult {
+  id: Number,
+  results: Game[]
+}
+
+interface Game {
+  id: Number,
+  name: String
+}
+
+const setGames = () => {
+  return instance.get("/games")
+}
 
 function App() {
-
+  const [games, setGames] = useState<Game[]>([])
+  useEffect (() => {
+    instance.get<GameResult>("/games")
+    .then((response)=> setGames(response.data.results))
+    .catch((error) => console.log(error))
+  },[])
   return (
     <Grid
       templateAreas={`"header header"
@@ -16,13 +45,16 @@ function App() {
       fontWeight='bold'
     >
       <GridItem pl='2' bg='orange.300' area={'header'}>
-        <NavBar/>
+        <NavBar />
       </GridItem>
       <GridItem pl='2' bg='pink.300' area={'nav'}>
         Nav
       </GridItem>
       <GridItem pl='2' bg='green.300' area={'main'}>
-        Main
+        <ul>
+          <li>test</li>
+          {games.map(g => (<li>{g.name}</li>))}
+        </ul>
       </GridItem>
       <GridItem pl='2' bg='blue.300' area={'footer'}>
         Footer
--- rawg/src/main.tsx
+++ rawg/src/main.tsx
@@ -8,5 +8,5 @@ createRoot(document.getElementById('root')!).render(
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </StrictMode>,
+  </StrictMode>
 )
